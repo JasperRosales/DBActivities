dd146f625103938a43d7f5e29c0ec464b9cec82d
Asper The Ghost <23-19300@g.batstate-u.edu.ph>
2024-11-22 17:49:00 +0800

    Add files via upload

---
+++ SARI_SARI_STORE_ACT.sql
@@ -0,0 +1,39 @@
+-- CREATE schema SARISARISTORE;
+
+-- CREATE TABLE Products (
+--     Prod_id INT PRIMARY KEY,
+--     Prod_name VARCHAR(100),
+--     Prod_price DECIMAL(10, 2),
+--     Prod_quantity INT,
+--     Prod_category VARCHAR(50)
+-- );
+
+-- INSERT INTO Products (Prod_id, Prod_name, Prod_price, Prod_quantity, Prod_category) VALUES
+-- (1, 'Mang Juan Chips', 25, 12, 'Snacks'),
+-- (2, 'Pinoy Spaghetti Sauce', 15.5, 8, 'Sauces'),
+-- (3, 'Buko Juice', 40.75, 15, 'Beverages'),
+-- (4, 'Sinigang Mix', 10, 10, 'Seasonings'),
+-- (5, 'Lucky Me! Instant Noodles', 35, 20, 'Instant Noodles'),
+-- (6, 'Sari-Sari Candies', 20, 5, 'Snacks'),
+-- (7, 'Nescafe 3-in-1', 18.25, 7, 'Beverages'),
+-- (8, 'Coco Mama', 30, 6, 'Cooking Oil'),
+-- (9, 'Panda Ice Cream', 12.5, 5, 'Ice Cream'),
+-- (10, 'Skyflakes Crackers', 22, 11, 'Snacks'),
+-- (11, 'Tuna Flakes in Oil', 50, 9, 'Canned Goods'),
+-- (12, 'Crispy Fry', 8.75, 4, 'Breading Mix'),
+-- (13, 'Pineapple Juice', 45, 8, 'Beverages'),
+-- (14, 'Del Monte Spaghetti Sauce', 75, 5, 'Sauces'),
+-- (15, 'Oishi Prawn Crackers', 28.3, 12, 'Snacks'),
+-- (16, 'Biskwit Biscuits', 35, 3, 'Snacks'),
+-- (17, 'Pawikan Fish Sauce', 60, 6, 'Condiments'),
+-- (18, 'Purefoods Hotdogs', 11.99, 20, 'Canned Goods'),
+-- (19, 'Vita Soy', 27.5, 7, 'Beverages'),
+-- (20, 'Lucky Me! Pancit Canton', 18, 14, 'Instant Noodles'),
+-- (21, 'Lemonade Powder', 33.8, 10, 'Drink Mix'),
+-- (22, 'Chocnut', 7.25, 5, 'Snacks'),
+-- (23, 'Milo', 2.5, 5, 'Beverages'),
+-- (24, 'Toblerone', 17.9, 6, 'Chocolate'),
+-- (25, 'Sampaguita Incense', 4.99, 2, 'Incense');
+
+
+SELECT * FROM Products;
